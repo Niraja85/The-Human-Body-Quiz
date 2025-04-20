8482b437bfc28a612187eb6da9f50031d29b1a2b
Niraja85 <tripathi.niraja@gmail.com>
2025-04-21 01:20:51 +0200

    Created a vrtual environment and added requiremnts.txt file

---
+++ run.py
@@ -0,0 +1,71 @@
+"""
+A welcome statement for starting the quiz and if the user
+says no, the game quits, and if chooses yes to play the game, 
+questions are displayed.
+"""
+print("Welcome to The Human Body Quiz")
+ans = input("Are you ready to play?\n").lower()
+
+if ans != "yes":
+    quit()
+
+q1 = """ Because its cells has mitochondria,which of your muscles never tires?
+a. Tongue
+b. Triceps
+c. Heart
+d. Gluteus Maximus
+"""
+q2 = """ The paper-thin tympanic membrane can be found in what body part?
+a. Ear
+b. Colon
+c. Lungs
+d. Throat
+"""
+q3 = """ Which of these body parts has the most bones?
+a. Rib-cage
+b. Foot
+c. Skull
+d. Hand
+"""
+q4 = """ Where are red blood cells created?
+a. Brain
+b. Heart
+c. Spleen
+d. Bones
+"""
+q5 = """ Which of these body parts continue to get bigger with age?
+a. Ear
+b. Thumb
+c. Spine
+d. Femur
+"""
+q6 = """ The funny bone is actually what type of body-part?
+a. Nerve
+b. Muscle
+c. Bone
+d. Organ
+"""
+q7 = """ How many bones are there in human body?
+a. 260
+b. 206
+c. 150
+d. 200
+"""
+q8 = """ What does mucus do?
+a. Filters out harmful bacteria.
+b. Helps in smell
+c. Helps your lungs take in oxygen
+d. Stops your nose from collapsing
+"""
+q9 = """ The most common eye color in the world is:
+a. Brown
+b. Blue
+c. Green
+d. Hazel
+"""
+q10 = """ How many muscles do you use when you talk?
+a. 45
+b. 55
+c. 72
+d. 87
+"""
